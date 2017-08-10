f7e3069ac841695521199b2edfcc03c9e6c5a8cd
Kafil Uddin Kiron <kukiron@users.noreply.github.com>
2017-08-10 21:18:37 +0600

    Edit test file

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -6,4 +6,6 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+
+  if(App.state) expect(App.state.length).toEqual(0);
 });
